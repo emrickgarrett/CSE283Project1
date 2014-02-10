9c9deb8b39488d34664acbfed68c09f60af92b1f
Garrett <emrickgj@miamioh.edu>
2014-02-10 12:09:57 -0500

    Init config

---
+++ Project1/src/BattleshipClient.java
@@ -0,0 +1,13 @@
+
+public class BattleshipClient {
+	
+	public BattleshipClient(){
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new BattleshipClient();
+	}
+
+}
